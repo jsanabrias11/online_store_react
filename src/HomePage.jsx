4e5b690a2eb6d8d0f039a333d89a50aa98edeec7
José E. Sanabria <109550083+jsanabrias11@users.noreply.github.com>
2022-11-26 14:34:39 -0300

    Update HomePage.jsx

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -5,6 +5,7 @@ import Container from '@mui/material/Container'
 export const HomePage = () => {
   return (    
     <Container maxWidth="lg">
+      <h1>Prueba</h1>
       <ProductsList />
     </Container>  
         
